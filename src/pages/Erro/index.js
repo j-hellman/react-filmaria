3a2b2bd89b9d22e2c5f841fc368a8d93995201c4
Jaime Hellman <jaime_hellman@hotmail.com>
2021-07-29 23:44:23 -0300

    pagina erro estilizada

--- src/pages/Erro/index.js
+++ src/pages/Erro/index.js
@@ -1,9 +1,12 @@
 import './erro.css';
+import { Link } from 'react-router-dom';
 
 export default function Erro(){
   return(
-    <div>
+    <div className="not-found">
       <h1>404</h1>
+      <h2>Desculpe, página não encontrada!</h2>
+      <Link to="/">Ver todos os filmes</Link>
     </div>
   );
 }
