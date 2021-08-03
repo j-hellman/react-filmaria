41044e5267970148e3ced7624e828e57fe2558db
Jaime Hellman <jaime_hellman@hotmail.com>
2021-08-03 00:42:47 -0300

    styled-component erro page

--- src/pages/Erro/index.js
+++ src/pages/Erro/index.js
@@ -1,12 +1,23 @@
-import './erro.css';
+// import './erro.css';
 import { Link } from 'react-router-dom';
+import { NotFound } from '../../styles';
 
 export default function Erro(){
   return(
-    <div className="not-found">
+    <NotFound>
       <h1>404</h1>
       <h2>Desculpe, página não encontrada!</h2>
       <Link to="/">Ver todos os filmes</Link>
-    </div>
+    </NotFound>
   );
 }
+
+// export default function Erro(){
+//   return(
+//     <div className="not-found">
+//       <h1>404</h1>
+//       <h2>Desculpe, página não encontrada!</h2>
+//       <Link to="/">Ver todos os filmes</Link>
+//     </div>
+//   );
+// }
---
+++ src/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+// Styled component para a pagina Erro
+export const NotFound = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1{
+    font-size: 80px;
+  }
+
+ a{
+    text-decoration: none;
+    color: #fff;
+    font-weight: bold;
+    background-color: red;
+    padding: .7rem;
+    margin-top: 20px;
+  }
+`;
+
+
+
+
